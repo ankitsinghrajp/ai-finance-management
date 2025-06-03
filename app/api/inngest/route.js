42e83ecf57002c431f32b4bdd8ca8cf3983cdcbe
ankit singh chouhan <ankitsinghchouhan682@gmail.com>
2025-06-03 19:47:04 +0530

    Created inngest function for sending monthly report and ai insights for the finance management

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,11 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts } from "@/lib/inngest/functions";
+import { checkBudgetAlerts, generateMonthlyReports } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
    checkBudgetAlerts,
+   generateMonthlyReports
   ],
 });
